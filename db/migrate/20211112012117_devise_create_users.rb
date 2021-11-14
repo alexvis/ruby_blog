6d6cd6a032efeae7a5eed83b53de04bd8dae1f3e
Alex Visconde <andres.visconde@gmail.com>
2021-11-13 23:46:04 -0300

    add style to blog page

--- db/migrate/20211112012117_devise_create_users.rb
+++ db/migrate/20211112012117_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       ## Database authenticatable
       t.boolean :admin, default: false
-      t.string :name, null: false
+      t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
