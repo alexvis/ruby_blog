c55dd877a902d3b04694bbce577cac27db74e9de
alexvis <andres.visconde@gmail.com>
2021-11-15 21:42:04 -0300

    add code to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :profile_photo, ProfilePhotoUploader
-  
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
 
   def full_name
